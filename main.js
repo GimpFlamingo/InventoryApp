2404a7a2044eaeb706f554a4dc45ea58e3672524
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-17 19:31:21 -0400

    Changed from deafault tabs.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -14,9 +14,16 @@ function createWindow () {
       nodeIntegration: true
     }
   })
+    
+
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+
+  Menu.setApplicationMenu(mainMenu)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -50,3 +57,10 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File'
+  }
+]
