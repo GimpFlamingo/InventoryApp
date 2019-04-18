6c438dff690bb420344441464e925a01bb34efe3
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-17 22:18:54 -0400

    Addes pg-promise and started setting up node.js connection to database.

--- addWindow.js
+++ addWindow.js
@@ -2,3 +2,5 @@
  * Takes the text entered in the addWindow.html form and adds
  * them to the relevant coloumn of the Postgres database.
  */ 
+var pgp = require('pg-promise')
+var db = pgp('postgres://postgres:InVent329R@host/database') // Fix this
