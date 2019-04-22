e7873c0f0642622ab87d3d993cd7aea6fa1e1ddb
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-22 08:47:25 -0400

    Reverted

--- addItemWindow.js
+++ addItemWindow.js
@@ -2,21 +2,11 @@
  * Takes the text entered in the addWindow.html form and adds
  * them to the relevant coloumn of the Postgres database.
  */
-const pg = require('pg')
-const add = document.querySelector('#add-part')
-const connectionString = 'postgres://postgres:ridgeback@localhost:5432/InventoryApp'
-
-var client = new pg.Client(connectionString)
-
-client.connect()
-
-
 var partId = document.getElementById('prod-id')
 var vendId = document.getElementById('manufact-id')
 var price = document.getElementById('manufact-cost')
 var desc = document.getElementById('description')
 var quant = document.getElementById('quantity')
-var date = new Date(year, monthIndex[day])
 
 var values = [req.query.partId, req.query.vendId, req.query.price, req.query.desc, req.query.quant, req.query.dateAdded, req.query.dateUpdated]
 
@@ -26,8 +16,17 @@ add.onClick() = function() {
     values, function (err, result) {
         console.log(err, result)
         res.end('success')
+    db.connect(function (req, res) {
+        var partId = document.getElementById('prod-id')
+        var vendId = document.getElementById('manufact-id')
+        if (err) throw err
+        console.log('Connected!')
+        var sql = 'INSERT INTO Inventory (item_id, vend_id, item_price, item_desc, item_quantity, date_added, last_update) '
+        sql += 'VALUES (' + partId + ', ' + vendId + ', ' + price + ', ' + desc + ', ' + quant + ', '
+        sql += new Date(year) + '-' + new Date(month) + '-' + new Date(day) + ');'
+        db.query(sql, function (err, result) {
+            console.log(err, result)
+            res.end('success')
+        })
     })
 }
-
-
-
