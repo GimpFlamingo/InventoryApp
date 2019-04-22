4baa3d6247d94d779617593f3bb158c480c8b280
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-21 21:48:28 -0400

    Made changes
    Postgres querrying is still not working.

--- addItemWindow.js
+++ addItemWindow.js
@@ -2,26 +2,31 @@
  * Takes the text entered in the addWindow.html form and adds
  * them to the relevant coloumn of the Postgres database.
  */
-var pgp = require('pg-promise')
-var db = pgp('postgres://postgres:ridgeback@localhost:5432/InventoryApp') // Fix this
-var add = document.querySelector('add-part')
+const pg = require('pg')
+const add = document.querySelector('add-part')
+const connectionString = 'postgres://postgres:ridgeback@localhost:5432/InventoryApp'
 
-add.onclick = function () {
+var client = new pg.Client(connectionString)
 
-    db.connect(function (req, res) {
-        var partId = document.getElementById('prod-id')
-        var vendId = document.getElementById('manufact-id')
-        var price = document.getElementById('manufact-cost')
-        var desc = document.getElementById('description')
-        var quant = document.getElementById('quantity')
-        if (err) throw err
-        console.log('Connected!')
-        var sql = 'INSERT INTO Inventory (item_id, vend_id, item_price, item_desc, item_quantity, date_added, last_update) '
-        sql += 'VALUES (' + partId + ', ' + vendId + ', ' + price + ', ' + desc + ', ' + quant + ', '
-        sql += new Date(year) + '-' + new Date(month) + '-' + new Date(day) + ');'
-        db.query(sql, function (err, result) {
-            console.log(err, result)
-            res.end('success')
-        })
+client.connect()
+
+
+var partId = document.getElementById('prod-id')
+var vendId = document.getElementById('manufact-id')
+var price = document.getElementById('manufact-cost')
+var desc = document.getElementById('description')
+var quant = document.getElementById('quantity')
+var date = new Date(year, monthIndex[day])
+
+var values = [req.query.partId, req.query.vendId, req.query.price, req.query.desc, req.query.quant, req.query.dateAdded, req.query.dateUpdated]
+
+add.onClick() = function() {
+    client.query('INSERT INTO Inventory (item_id, vend_id, item_price, item_desc, item_quantity, date_added, last_update) VALUES($1, $2, $3, $4, $5, $6, $7);',
+    values, function (err, result) {
+        console.log(err, result)
+        res.end('success')
     })
 }
+
+
+
