ae522d970a73c061423244804e5953873f84eeb0
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-22 20:37:52 -0400

    Updated package.json

--- main.js
+++ main.js
@@ -146,7 +146,7 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'New Purchase Order'
+                label: 'New Purchase Order',
             },
             {
                 label: 'Settings'
