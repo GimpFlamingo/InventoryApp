92b679e4f900da0c6d70a2219d2d94bd9e1f1853
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-23 19:07:20 -0400

    Removed README2 and moved files.
    README2 was part of the electron-quick-start that wasn't necessary anymore and organized folders to be grouped by location in tab.

---
+++ fileWindows/addItemWindow.js
@@ -0,0 +1,25 @@
+const { Client } = require('pg')
+const connectionString = 'postgresql://postgres:ridgeback@localhost:5432/inventoryapp'
+const addButton = document.getElementById('add-part')
+
+const client = new Client(connectionString)
+
+client.connect()
+
+addButton.onclick = function () {
+    console.log('Click!')
+    var partId = document.getElementById('prod-id')
+    var vendId = document.getElementById('manufact-id')
+    var price = document.getElementById('manufact-cost')
+    var desc = document.getElementById('description')
+    var quant = document.getElementById('quantity')
+    var values = [partId.value, vendId.value, price.value, desc.value, quant.value]
+    if (err) throw err
+    console.log('Connected!')
+    client.query('INSERT INTO public.inventory (item_id, vend_id, item_price, item_desc, item_quantity) VALUES ($1, $2, $3, $4, $5);', values, (err, res) => {
+        console.log(err, result)
+        res.end('success')
+        client.end()
+    })
+
+}
