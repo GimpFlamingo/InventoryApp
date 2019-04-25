1ae9ebc7949e4da100639eda1349db2620e38b00
Raybird9 <rayjkochenderfer@gmail.com>
2019-04-25 10:18:55 -0400

    Worked on editItemWindow

--- editWindows/editItemWindow.js
+++ editWindows/editItemWindow.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg')
 const connectionString = 'postgresql://postgres:ridgeback@localhost:5432/inventoryapp'
+
 window.onload = function () {
     // User only has to enter one of the following variables to search the database
     // The Part ID will hold priority over the part name
@@ -9,12 +10,26 @@ window.onload = function () {
 
     client.connect()
 
+    var partID = document.getElementById('part-id')
+    var partName = document.getElementById('part-name')
+    var modal = document.getElementById('id-finder')
+    var display = document.getElementById('display-part')
+
     searchButton.onclick = function () {
         // Add functionality that if part is not in database it shows error that part-id or part 
         // name is invalid. Make sure modal is hidden after a valid id/name is entered
-        console.log('Click!')
-        var partID = document.getElementById('part-id')
-        var partName = document.getElementById('part-name')
-    
+
+        // If a valid input is given hide the modal and display the querry results
+        if (partID.value === "") {
+            window.alert("Please enter a valid PartID")
+        } else {
+            var partElement = document.querySelector('#part')
+            var sql = 'SELECT * FROM public.inventory WHERE item_id = ' + partID.value + ';'
+            var values = []
+            client.query(sql, (err, res) => {
+                
+            })
+        }
+
     }
 }
--- fileWindows/addCustomerWindow.js
+++ fileWindows/addCustomerWindow.js
@@ -19,11 +19,14 @@ window.onload = function () {
         var custPhone = document.getElementById('customerPhoneInput')
         var custEmail = document.getElementById('customerEmailInput')
 
-        var values = [custId.value, custName.value, custAddress.value, custCity.value, custState.value, custZip.value, custCountry.value, custPhone.value, custEmail.value]        
-        client.query('INSERT INTO public.customers (cust_id, cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_phone, cust_email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);', values, (err, res) => {
-            console.log(err, res)
-            client.end()
-        })
-
+        if (cust_id.value === "" || cust_name.value === "" || cust_address.value === "" || cust_city.value === "" || cust_zip.value === "" || cust_email.value === "") {
+            window.alert("The following fields must be filled: Customer ID, Customer Name, Customer Address, Customer City, Customer ZIP code, Customer E-mail.")
+        } else {
+            var values = [custId.value, custName.value, custAddress.value, custCity.value, custState.value, custZip.value, custCountry.value, custPhone.value, custEmail.value]
+            client.query('INSERT INTO public.customers (cust_id, cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_phone, cust_email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);', values, (err, res) => {
+                console.log(err, res)
+                client.end()
+            })
+        }
     }
 }
