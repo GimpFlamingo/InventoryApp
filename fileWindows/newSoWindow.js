ac7dce00a8dbbc908a015a08de3dfc56b811d74a
Raybird9 <rayjkochenderfer@gmail.com>
2019-05-08 21:49:36 -0400

    Finished adding functionality to delete rows from the part list table

--- fileWindows/newSoWindow.js
+++ fileWindows/newSoWindow.js
@@ -10,7 +10,6 @@ window.onload = function () {
     const createButton = document.getElementById('create-order')
     const addPartButton = document.getElementById('add-part')
     const finishAddingButton = document.getElementById('finish-adding')
-    var deleteItemButton = document.getElementsByClassName('delete')
     // Buttons
 
     // Displays
@@ -25,17 +24,43 @@ window.onload = function () {
     const header = document.getElementById('header')
     const custHead = document.getElementById('customer-header')
 
+    var itemTable = document.getElementById('item-table')
     var custId
     var totalCost = 0
     var totalItems = 0
-    var temp = ''
     var items = []
 
     function deleteRow(btn) {
-        console.log(btn)
-        var row = btn.parentNode.parentNode
+        row = btn.parentNode.parentNode.parentNode
+        // Delete HTML table row
         row.parentNode.removeChild(row)
+        var i = 0
+        var found = false
+        // Find index value of item
+        while (i < items.length && !found) {
+            if (items[i].itemId == row.firstChild.innerHTML) {
+                found = true
+            } else {
+                i++
+            }
+        }
+        if (found) {
+            // Adjust global values
+            totalCost -= items[i].totalCost
+            totalItems -= items[i].amount
+            // Write to HTML
+            totalCostDisplay.innerHTML = '$' + totalCost.toFixed(2)
+            totalItemDisplay.innerHTML = totalItems
+            // Remove element
+            items.splice(i, 1)
+            console.log(items)
+        } else {
+            console.log('Something is wrong')
+        }
+
     }
+
+
 
     // Button to create sales order
     createButton.onclick = () => {
@@ -55,41 +80,61 @@ window.onload = function () {
         })
     }
 
-    deleteItemButton.onclick = () => {
-        console.log(btn)
-        var row = btn.parentNode.parentNode
-        row.parentNode.removeChild(row)
-    }
-
-
     // Button to add part to the sales order
     addPartButton.onclick = () => {
+        var found = false
         var enteredNum = document.getElementById('part-count')
-        var addPart = document.getElementById("part-id")
-
-        // Add way to check what parts need to be ordered in the inventory based off what is currently in stock
-        client.query('SELECT item_price FROM public.inventory WHERE item_id=\'' + addPart.value + '\';', (err, res) => {
-            if (err !== null) {
-                console.log(err)
-                window.alert("Error adding part")
-            } else if (res.rows[0] == null) {
-                window.alert("This part is not in inventory")
-            } else {
-                // Add part to list
-                temp += '<tr><th scope="row">' + addPart.value + '</th><td>' + enteredNum.value + '</td><td><button type="button" value="Delete" class="delete"><span>&times;</span></button></td></tr>'
-                items.push({ 'itemId': addPart.value, 'amount': Number(enteredNum.value) })
-                console.log(items)
-                // Increase total cost and total items
-                totalCost = Number(totalCost) + Number(res.rows[0].item_price) * Number(enteredNum.value)
-                totalItems += Number(enteredNum.value)
-                // Display part to list
-                partList.innerHTML = temp
-                totalCostDisplay.innerHTML = '$' + totalCost.toFixed(2)
-                totalItemDisplay.innerHTML = totalItems
-                addPart.value = ''
-                enteredNum.value = ''
+        var addPart = document.getElementById('part-id')
+        var x = 0
+        // Check if part is already in the list
+        while (x < items.length && !found) {
+            if (items[x].itemId === addPart.value) {
+                found = true
             }
-        })
+            x++
+        }
+        if (!found) {
+            // Add way to check what parts need to be ordered in the inventory based off what is currently in stock
+            client.query('SELECT item_price FROM public.inventory WHERE item_id=\'' + addPart.value + '\';', (err, res) => {
+                if (err !== null) {
+                    console.log(err)
+                    window.alert('Error adding part')
+                } else if (res.rows[0] == null) {
+                    window.alert('This part is not in inventory')
+                } else {
+                    // Add part to list
+                    // Think about changing delete icon to a trash can icon
+                    // Maybe add an edit icon to allow the user to edit the amount added instead of deleting and retyping
+                    var temp = '<tr><th scope="row">' + addPart.value + '</th><td>' + enteredNum.value + '</td><td><button type="button" class="close" aria-label="Delete"><span class="delete" aria-hidden="true">&times;</span></button></td></tr>'
+                    // Add entry to global parts list
+                    var tempCost = Number(res.rows[0].item_price) * Number(enteredNum.value)
+                    items.push({ 'string': temp, 'itemId': addPart.value, 'amount': Number(enteredNum.value), 'totalCost': tempCost })
+                    // Add event listener to delete row if necessary
+                    itemTable.addEventListener('click', function (e) {
+                        if (e.target.className == 'delete') {
+                            deleteRow(e.target)
+                        }
+                    }, false)
+                    // Increase total cost and total items
+                    totalCost += tempCost
+                    totalItems += Number(enteredNum.value)
+                    // Display part to list
+                    var addedParts = ''
+                    // Build addedParts by pulling the strings from the array of parts
+                    for (var i = 0; i < items.length; i++) {
+                        addedParts += items[i].string
+                    }
+                    // Build HTML
+                    partList.innerHTML = addedParts
+                    totalCostDisplay.innerHTML = '$' + totalCost.toFixed(2)
+                    totalItemDisplay.innerHTML = totalItems
+                    addPart.value = ''
+                    enteredNum.value = ''
+                }
+            })
+        } else {
+            window.alert('This part is already in the list')
+        }
     }
 
     // Button to finish adding parts to the sales order
@@ -111,6 +156,12 @@ window.onload = function () {
     }
 
     // Add section that display in review area that shows the user what parts need to be ordered to complete the order as created
+
+    itemTable.addEventListener('click', function (e) {
+        if (e.target.className == 'close') {
+            deleteRow(e)
+        }
+    }, false)
 }
 
 window.onbeforeunload = () => {
