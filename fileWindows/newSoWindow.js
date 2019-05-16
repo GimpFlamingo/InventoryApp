089dd60486d320775624ed2f44e537966e95c7d4
Raybird9 <rayjkochenderfer@gmail.com>
2019-05-15 21:32:03 -0400

    When SO is finish all information is added to databse.

--- fileWindows/newSoWindow.js
+++ fileWindows/newSoWindow.js
@@ -10,12 +10,15 @@ window.onload = function () {
     const createButton = document.getElementById('create-order')
     const addPartButton = document.getElementById('add-part')
     const finishAddingButton = document.getElementById('finish-adding')
+    const finishShippingButton = document.getElementById('finish-shipping')
+    const finishReview = document.getElementById('finish-review')
     // Buttons
 
     // Displays
     const modal = document.getElementById('id-finder')
     const partListSection = document.getElementById('part-adder')
     const shipScreenSection = document.getElementById('ship-screen')
+    const review = document.getElementById('order-review')
     // Displays
 
     const partList = document.getElementById('part-list')
@@ -29,22 +32,25 @@ window.onload = function () {
     var totalCost = 0
     var totalItems = 0
     var items = []
+    var shippingInfo = []
+    var salesNum
+    var jobName
 
     function deleteRow(btn) {
         row = btn.parentNode.parentNode.parentNode
         // Delete HTML table row
         row.parentNode.removeChild(row)
         var i = 0
-        var found = false
+        var inList = false
         // Find index value of item
-        while (i < items.length && !found) {
+        while (i < items.length && !inList) {
             if (items[i].itemId == row.firstChild.innerHTML) {
-                found = true
+                inList = true
             } else {
                 i++
             }
         }
-        if (found) {
+        if (inList) {
             // Adjust global values
             totalCost -= items[i].totalCost
             totalItems -= items[i].amount
@@ -64,9 +70,9 @@ window.onload = function () {
 
     // Button to create sales order
     createButton.onclick = () => {
-        custId = document.getElementById("cust-id")
+        custId = document.getElementById("cust-id").value
 
-        client.query('SELECT cust_id, cust_name FROM public.customers WHERE cust_id=\'' + custId.value + '\';', (err, res) => {
+        client.query('SELECT cust_id, cust_name FROM public.customers WHERE cust_id=\'' + custId + '\';', (err, res) => {
             console.log(err, res)
             if (res.rows[0] == null) {
                 console.log(err)
@@ -78,22 +84,23 @@ window.onload = function () {
                 custHead.innerHTML = '<u>Shipping for ' + res.rows[0].cust_name + ' (' + res.rows[0].cust_id + ')</u>'
             }
         })
+        jobName = custId
     }
 
     // Button to add part to the sales order
     addPartButton.onclick = () => {
-        var found = false
+        var inList = false
         var enteredNum = document.getElementById('part-count')
         var addPart = document.getElementById('part-id')
         var x = 0
         // Check if part is already in the list
-        while (x < items.length && !found) {
+        while (x < items.length && !inList) {
             if (items[x].itemId === addPart.value) {
-                found = true
+                inList = true
             }
             x++
         }
-        if (!found) {
+        if (!inList && enteredNum.value > 0) {
             // Add way to check what parts need to be ordered in the inventory based off what is currently in stock
             client.query('SELECT item_price FROM public.inventory WHERE item_id=\'' + addPart.value + '\';', (err, res) => {
                 if (err !== null) {
@@ -105,7 +112,7 @@ window.onload = function () {
                     // Add part to list
                     // Think about changing delete icon to a trash can icon
                     // Maybe add an edit icon to allow the user to edit the amount added instead of deleting and retyping
-                    var temp = '<tr><th scope="row">' + addPart.value + '</th><td>' + enteredNum.value + '</td><td><button type="button" class="close" aria-label="Delete"><span class="delete" aria-hidden="true">&times;</span></button></td></tr>'
+                    var temp = '<tr><th scope="row">' + addPart.value + '</th><td>' + enteredNum.value + '</td>'
                     // Add entry to global parts list
                     var tempCost = Number(res.rows[0].item_price) * Number(enteredNum.value)
                     items.push({ 'string': temp, 'itemId': addPart.value, 'amount': Number(enteredNum.value), 'totalCost': tempCost })
@@ -118,11 +125,10 @@ window.onload = function () {
                     // Increase total cost and total items
                     totalCost += tempCost
                     totalItems += Number(enteredNum.value)
-                    // Display part to list
                     var addedParts = ''
                     // Build addedParts by pulling the strings from the array of parts
                     for (var i = 0; i < items.length; i++) {
-                        addedParts += items[i].string
+                        addedParts += items[i].string + '<td><button type="button" class="close" aria-label="Delete"><span class="delete" aria-hidden="true">&times;</span></button></td></tr>'
                     }
                     // Build HTML
                     partList.innerHTML = addedParts
@@ -132,38 +138,99 @@ window.onload = function () {
                     enteredNum.value = ''
                 }
             })
+        } else if (enteredNum.value === 0) {
+            window.alert('0 is an invalid item amount.')
+        } else if (enteredNum.value == null) {
+            window.alert('You must enter an item amount.')  
         } else {
-            window.alert('This part is already in the list')
+            window.alert('This part is already in the list.')
         }
     }
 
     // Button to finish adding parts to the sales order
     finishAddingButton.onclick = () => {
-        var salesNum
         // Find the sale number
         client.query('SELECT COUNT(*) AS sales_num FROM orders', (err, res) => {
             console.log(err, res)
             salesNum = Number(res.rows[0].sales_num)
         })
 
-        // Need to start a client.query that requires all parts of the order and order items to be entered
-        // for the order to be saved to the database (commit queries). Need to add the order to the orders table
-        // and the order_items table. Make sure all fields are filled in order to be added to database.
-
         // Reveal the shipping screen and hide part list screen
         partListSection.style.display = 'none'
         shipScreenSection.style.display = 'block'
     }
 
-    // Add section that display in review area that shows the user what parts need to be ordered to complete the order as created
-
-    itemTable.addEventListener('click', function (e) {
-        if (e.target.className == 'close') {
-            deleteRow(e)
+    finishShippingButton.onclick = () => {
+        var partReview = document.getElementById('part-review')
+        var shippingReview = document.getElementById('shipping-review')
+        var totalReview = document.getElementById('total-review')
+        var finalList = ''
+        shippingInfo = {
+            'poNum': document.getElementById('po-number').value,
+            'shipVia': document.getElementById('ship-via').value,
+            'shipAddress': document.getElementById('ship-to-address').value,
+            'shipCity': document.getElementById('ship-to-city').value,
+            'shipZip': document.getElementById('ship-zip').value,
+            'shipState': document.getElementById('ship-state').value
         }
-    }, false)
+        // TODO: Check to make sure the entered shipping information is valid with the customers information
+        // Build final part list
+        for (var i = 0; i < items.length; i++) {
+            finalList += items[i].string
+        }
+        // Build table containing the shipping information
+        var shippingTable = '<tr><th scope="row">PO Number</th><td>' + shippingInfo.poNum + '</td></tr>'
+        shippingTable += '<tr><th scope="row">Sales Order Number</th><td>' + salesNum + '</td></tr>'
+        shippingTable += '<tr><th scope="row">Shipping Via</th><td>' + shippingInfo.shipVia + '</td></tr>'
+        shippingTable += '<tr><th scope="row">Shipping Address</th><td>' + shippingInfo.shipAddress + ' ' + shippingInfo.shipCity + ', ' + shippingInfo.shipState + ' ' + shippingInfo.shipZip + '</td></tr>'
+        // Build total information
+        var totals = '<tr><td scope="row">' + totalItems + '</td><td>' + totalCost + '</td></tr>'
+        // Build HTML
+        partReview.innerHTML = finalList
+        shippingReview.innerHTML = shippingTable
+        totalReview.innerHTML = totals
+        // Hide ship screen and show review page
+        shipScreenSection.style.display = 'none'
+        review.style.display = 'block'
+    }
+
+    finishReview.onclick = () => {
+        var proceed = window.confirm('Press ok if all the details are correct.')
+        if (proceed) {
+            // Add information to orders table
+            var orderLog = [salesNum, shippingInfo.poNum, custId, totalCost, totalCost, jobName]
+            var orderSql = 'INSERT INTO public.orders' + 
+                '(sales_order_num, purchase_order_num, cust_id, quoted_price, sale_price, job_name) ' + 
+                'VALUES ($1, $2, $3, $4, $5, $6);'
+            
+            client.query(orderSql, orderLog, (err, res) => {
+                console.log(err, res)
+            })
+            // Add information to order_items table
+            for (var i = 0; i < items.length; i++) {
+                var itemsSql = 'INSERT INTO public.order_items(' +
+                'sales_order_num, item_amount, part_id, total_price, multiplier_price)' + 
+                'VALUES ($1, $2, $3, $4, $5);'
+                var itemsLog = [salesNum, items[i].amount, items[i].itemId, items[i].totalCost, items[i].totalCost * 1.5]
+                client.query(itemsSql, itemsLog, (err, res) => {
+                    console.log(err, res)
+                })
+            }
+            var shippingSql = 'INSERT INTO public.shipping' +
+                '(sales_order_num, purchase_order_num, ship_via, ship_to_address, ship_to_city, ship_to_zip, ship_to_state, cust_id)' +
+                'VALUES ($1, $2, $3, $4, $5, $6, $7, $8);'
+            var shippingLog = [salesNum, shippingInfo.poNum, shippingInfo.shipVia, shippingInfo.shipAddress, shippingInfo.shipCity, shippingInfo.shipZip, shippingInfo.shipState, custId]
+            // Add information to shipping tool
+            client.query(shippingSql, shippingLog, (err, res) => {
+                console.log(err, res)
+            })
+            
+            window.alert('The order has been submited!')
+            //document.location.reload()
+        }
+    }
 }
 
-window.onbeforeunload = () => {
+window.onunload = () => {
     client.end()
 }
